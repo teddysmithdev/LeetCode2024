049b20c80c1d5ff3d31c421b46d38db536cb0770
teddysmithdev <teddysmithdev@gmail.com>
2024-06-28 08:27:37 -0400

    Group Anagrams

--- Solution.cs
+++ Solution.cs
@@ -1,20 +1,46 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace LeetCode2024
 {
     public class Solution
     {
-        public int SingleNumber(int[] nums)
+        public IList<IList<string>> GroupAnagrams(string[] strs)
         {
-            int a = 0;
-            foreach (int i in nums)
+            if (strs.Length == 0)
             {
-                a ^= i;
+                return new List<IList<string>>();
             }
-            return a;
+
+            Dictionary<string, List<string>> anagrams = new();
+
+            foreach (string word in strs)
+            {
+                int[] count = new int[26];
+                foreach (char c in word)
+                {
+                    count[c - 'a']++;
+                }
+
+                StringBuilder sb = new();
+                for (int i = 0; i < 26; i++)
+                {
+                    sb.Append("#");
+                    sb.Append(count[i]);
+                }
+                string key = sb.ToString();
+
+                if (!anagrams.ContainsKey(key))
+                {
+                    anagrams[key] = new List<string>();
+                }
+
+                anagrams[key].Add(word);
+            }
+            return new List<IList<string>>(anagrams.Values);
         }
     }
 }
