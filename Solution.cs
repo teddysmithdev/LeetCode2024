c11e2f600d58122cc7e5e9f59bd49fda07ffa0bf
teddysmithdev <teddysmithdev@gmail.com>
2024-07-15 12:39:27 -0400

    Add Two Numbers

--- Solution.cs
+++ Solution.cs
@@ -7,26 +7,40 @@ using System.Threading.Tasks;
 
 namespace LeetCode2024
 {
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int val = 0, ListNode next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+    }
     public class Solution
     {
-        public int MinSubArrayLen(int target, int[] nums)
+        public ListNode AddTwoNumbers(ListNode l1, ListNode l2)
         {
-            int minLength = int.MaxValue;
-            int left = 0;
-            int currentSum = 0;
-
-            for (int right = 0; right < nums.Length; right++)
+            ListNode result = new();
+            ListNode head = result;
+            int sum = 0;
+            while (l1 != null || l2 != null || sum > 0)
             {
-                currentSum += nums[right];
-
-                while (currentSum >= target)
+                if (l1 != null)
                 {
-                    minLength = Math.Min(minLength, right - left + 1);
-                    currentSum -= nums[left];
-                    left++;
+                    sum += l1.val;
+                    l1 = l1.next;
                 }
+                if (l2 != null)
+                {
+                    sum += l2.val;
+                    l2 = l2.next;
+                }
+                result.next = new ListNode(sum % 10);
+                sum /= 10;
+                result = result.next;
             }
-            return minLength == int.MaxValue ? 0 : minLength;
+            return head.next;
         }
     }
 }
