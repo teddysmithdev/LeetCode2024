8bfb6b512ba8eb5a627e9fc43cedcc9c4f4cbe72
teddysmithdev <teddysmithdev@gmail.com>
2024-07-17 09:21:04 -0400

    Merge Sorted Array

--- Solution.cs
+++ Solution.cs
@@ -9,49 +9,23 @@ namespace LeetCode2024
 {
     public class Solution
     {
-        private Dictionary<char, int> MakeCountMap(string s)
+        public void Merge(int[] nums1, int m, int[] nums2, int n)
         {
-            Dictionary<char, int> counts = new();
-            foreach (char c in s)
+            int p1 = m - 1;
+            int p2 = n - 1;
+            int i = m + n - 1;
+
+            while (p2 >= 0)
             {
-                if (counts.ContainsKey(c))
+                if (p1 >= 0 && nums1[p1] > nums2[p2])
                 {
-                    counts[c]++;
+                    nums1[i--] = nums1[p1--];
                 }
                 else
                 {
-                    counts[c] = 1;
+                    nums1[i--] = nums2[p2--];
                 }
             }
-            return counts;
-        }
-
-        public bool CanConstruct(string ransomNote, string magazine)
-        {
-            if (ransomNote.Length > magazine.Length)
-            {
-                return false;
-            }
-
-            Dictionary<char, int> magazineCounts = MakeCountMap(magazine);
-
-            foreach (char c in ransomNote)
-            {
-                if (magazineCounts.TryGetValue(c, out int countInMagazine))
-                {
-                    if (countInMagazine == 0)
-                    {
-                        return false;
-                    }
-
-                    magazineCounts[c] = countInMagazine - 1;
-                }
-                else
-                {
-                    return false;
-                }
-            }
-            return true;
         }
     }
 }
